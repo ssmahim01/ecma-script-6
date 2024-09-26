0ca1254ab7b136757810f471d1ec907ddf4a191d
ssmahim01 <mahimzayn666@gmail.com>
2024-09-26 15:50:37 +0600

    Edited practice file of JavaScript

--- practice.js
+++ practice.js
@@ -5,10 +5,18 @@
 const multiplication2 = (num1, num2, num3) => num1 * num2 * num3;
 console.log(multiplication2(5, 6, 8));
 
+// Output => 240
+
 // First normal way
 
 const introduction = "I am a web developer." + "\n" + "I love to code." + "\n" + "I love to eat biryani.";
 console.log(introduction);
+
+/* Output => 
+I am a web developer.
+I love to code.
+I love to eat biryani.
+*/
 
 // Second easy way
 
@@ -19,6 +27,12 @@ I love to eat biryani.
 `
 console.log(multiLine);
 
+/* Output =>
+I am a web developer.
+i love to code.
+I love to eat biryani.
+*/
+
 // Default parameter
 
 const addition = (num1, num2 = 5) => num1 + num2;
@@ -26,7 +40,12 @@ const sumOfArrow = addition(5);
 const sumOfArrow2 = addition(5, 15);
 
 console.log(sumOfArrow);
+
+// Output => 10
+
 console.log(sumOfArrow2);
+
+// Output => 20
 
 /* Practice Task 2 */
 
@@ -47,6 +66,8 @@ const findEventLength = (friends) => {
 const myFriends = findEventLength(["MZ", "Rahim", "Rafi Khan Omor", "Jubayer"]);
 console.log(myFriends);
 
+// Output => [ 'MZ', 'Rafi Khan Omor' ]
+
 /* Practice Task 3 */
 
 // Average of squared elements
@@ -64,6 +85,8 @@ const averageSquaredElements = (numbers) => {
 const numbersArray = averageSquaredElements([2, 5, 6, 8, 10]);
 console.log(numbersArray);
 
+// Output => 45.8
+
 /* Practice Task 4 */
 
 // Find max number from the combined array
@@ -80,3 +103,13 @@ const secondArray = [20, 15, 80, 70, 60, 50];
 
 const checkResult = findMax(firstArray, secondArray);
 console.log(checkResult);
+
+/* First Output =>
+[
+   2,  5,  6,  7,  8, 16,
+  66, 10, 20, 15, 80, 70,
+  60, 50
+]
+*/
+
+// Second Output => 80
